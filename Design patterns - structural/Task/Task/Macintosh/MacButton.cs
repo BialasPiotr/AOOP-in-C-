d01c0306eaef84c286837d5e9851b2952e94dc75
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2024-03-16 15:47:39 +0100

    Task 2

---
+++ Design patterns - structural/Task/Task/Macintosh/MacButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Macintosh
+{
+    public class MacButton : IButton
+    {
+        public void Draw() => Console.WriteLine("Macintosh button has been drawn");
+    }
+}
