a895e1492ec7db082dc050ac831de6ab9d92d6d3
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2024-03-16 16:01:16 +0100

    added Task2

---
+++ Design patterns - structural/Task/Task/Macintosh/MacFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Macintosh
+{
+    public class MacFactory : IGuiFactory
+    {
+        public IButton CreateButton() => new MacButton();
+        public ICheckbox CreateCheckBox() => new MacCheckbox();
+    }
+}
---
+++ Design patterns - structural/Task/Task/Windows/WinFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Windows
+{
+    public class WinFactory : IGuiFactory
+    {
+        public IButton CreateButton() => new WinButton();
+        public ICheckbox CreateCheckBox() => new WinCheckbox();
+    }
+}
